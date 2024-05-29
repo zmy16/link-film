6b3fb7e2f63018591c2f71965a6b3d59607ac545
Muhammad Raid Zakwan <zmuhammadraid@gmail.com>
2024-05-29 21:38:34 +0700

    updated

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,11 +1,45 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const links = document.querySelectorAll('#film-sites a');
-    links.forEach(link => {
-        link.addEventListener('mouseover', () => {
-            link.style.color = '#ffffff';
-        });
-        link.addEventListener('mouseout', () => {
-            link.style.color = '#ffcc00';
-        });
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll("#film-sites a");
+  links.forEach((link) => {
+    link.addEventListener("mouseover", () => {
+      link.style.color = "#ffffff";
     });
+    link.addEventListener("mouseout", () => {
+      link.style.color = "#ffcc00";
+    });
+    link.addEventListener("click", () => {
+      link.innerHTML = "Loading...";
+    });
+  });
+});
+
+// Lazy loading images
+document.addEventListener("DOMContentLoaded", function () {
+  let lazyloadImages = document.querySelectorAll("img.lazy");
+  let lazyloadThrottleTimeout;
+
+  function lazyload() {
+    if (lazyloadThrottleTimeout) {
+      clearTimeout(lazyloadThrottleTimeout);
+    }
+
+    lazyloadThrottleTimeout = setTimeout(function () {
+      let scrollTop = window.pageYOffset;
+      lazyloadImages.forEach(function (img) {
+        if (img.offsetTop < window.innerHeight + scrollTop) {
+          img.src = img.dataset.src;
+          img.classList.remove("lazy");
+        }
+      });
+      if (lazyloadImages.length == 0) {
+        document.removeEventListener("scroll", lazyload);
+        window.removeEventListener("resize", lazyload);
+        window.removeEventListener("orientationChange", lazyload);
+      }
+    }, 20);
+  }
+
+  document.addEventListener("scroll", lazyload);
+  window.addEventListener("resize", lazyload);
+  window.addEventListener("orientationChange", lazyload);
 });
